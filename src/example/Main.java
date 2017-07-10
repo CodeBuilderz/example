208865e66dc4ba83ef01fb3f44fafeb1dc2bc5d2
fuse28 <donguk.choi@nhnent.com>
2017-07-10 14:57:35 +0900

    example answer

--- src/example/Main.java
+++ src/example/Main.java
@@ -4,12 +4,20 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
 
         List<Apple> appleList = new ArrayList<>();
+        appleList.add(new Apple(100, "RedApple", "Red"));
+        appleList.add(new Apple(150, "BlueApple", "Blue"));
+        appleList.add(new Apple(200, "YellowApple", "Yellow"));
+        appleList.add(new Apple(250, "RainbowApple", "Rainbow"));
 
         // 정렬
         Collections.sort(appleList, new Comparator<Apple>() {
@@ -18,6 +26,9 @@ public class Main {
             }
         });
 
+        Collections.sort(appleList, (p1, p2) -> p1.getName().compareTo(p2.getName()));
+        Collections.sort(appleList, Comparator.comparing(Apple::getName));
+
         // 쓰레드
         Runnable r1 = new Runnable() {
             @Override
@@ -25,8 +36,10 @@ public class Main {
                 System.out.println("Hello world one!");
             }
         };
-
         r1.run();
+
+        Runnable r2 = () -> System.out.println("Hello World one!");
+        r2.run();
 
         /*
         *  Stream 예제
@@ -39,6 +52,8 @@ public class Main {
                 System.out.println("button click!!!");
             }
         });
+
+        button.addActionListener(e -> System.out.println("button click!!!"));
 
         // 특정 항목 필터링
         List<Apple> filteringAppleList = new ArrayList<>();
@@ -53,6 +68,9 @@ public class Main {
         for (Apple apple : appleList) {
             appleMap.put(apple.getName(), apple);
         }
+        appleMap = appleList.stream()
+                .collect(Collectors.toMap(Apple::getName, Function.identity()));
+        System.out.println(appleMap.get("BlueApple"));
 
         // 중복되지 않은 정보 추출
         List<Apple> distinctAppleList = new ArrayList<>();
@@ -62,10 +80,22 @@ public class Main {
             }
         }
 
+        List<Apple> distinctApple2List = appleList.stream()
+                .distinct()
+                .collect(toList());
+        distinctApple2List.forEach(System.out::println);
+
         // 특정항목만 추출
         List<String> appleNameList = new ArrayList<>();
         for (Apple apple : appleList) {
             appleNameList.add(apple.getName());
         }
+
+        List<String> nameList = appleList.stream()
+                .map(Apple::getName)
+                .collect(toList());
+
+        nameList.forEach(System.out::println);
+        //
     }
 }
